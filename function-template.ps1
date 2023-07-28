d487e2da07d660d6230edab1e65a917de176493a
DG-ESF17 <102463539+DG-ESF17@users.noreply.github.com>
2023-07-27 19:13:01 -0500

    Back on track!!

--- function-template.ps1
+++ function-template.ps1
@@ -25,64 +25,42 @@ function get-function-template {
         Additional notes about the function.
     
     #>
-    [CmdletBinding()]
-    param(
-        [Parameter(Mandatory=$true)]
-        [string]$Parameter1,
-        [Parameter(Mandatory=$false)]
-        [string]$Parameter2 = "default value"
+    [
+        CmdletBinding(
+            SupportsShouldProcess=$True,
+            ConfirmImpact="Low"
+        )
+    ]
+    Param(
+        [Parameter(Mandatory=$True,
+        ValueFromPipeline=$True,
+        ValueFromPipelineByPropertyName=$True)]
+        [Object]$User
     )
-
-    try {
-        $result = $Parameter1 / $Parameter2
+    Begin {
+        Write-Verbose "Starting New-ExampleFunction"
+    }
+    Process {
+        Write-Verbose "Processing First Parameter: $Parameter1"
+        Write-Verbose "Performing operation"
+        try {
+            $result = $Parameter1 / $Parameter2
+            return $result
+        }
+            catch [System.DivideByZeroException] {
+                Write-Error "Cannot divide by zero."
+            }
+        #Verbose
+        Write-Verbose "Processing Second Parameter: $Parameter2"
+        catch [System.OverflowException] {
+            Write-Error "The result is too large to be represented as an integer."
+        }
+        catch {
+            Write-Error "An unexpected error occurred: $($_.Exception.Message)"
+        }
+    }
+    End {
+        Write-Verbose "New-ExampleFunction completed successfully"
         return $result
-    }
-    catch [System.DivideByZeroException] {
-        Write-Error "Cannot divide by zero."
-    }
-    catch [System.OverflowException] {
-        Write-Error "The result is too large to be represented as an integer."
-    }
-    catch {
-        Write-Error "An unexpected error occurred: $($_.Exception.Message)"
-    }
-}
-
-
-function Divide-Numbers {
-    param (
-        [Parameter(Mandatory=$true)]
-        [int]$Dividend,
-        [Parameter(Mandatory=$true)]
-        [int]$Divisor
-    )
-    try {
-        $result = $Dividend / $Divisor
-        return $result
-    }
-    catch [System.DivideByZeroException] {
-        Write-Error "Cannot divide by zero."
-    }
-    catch [System.OverflowException] {
-        Write-Error "The result is too large to be represented as an integer."
-    }
-    catch {
-        Write-Error "An unexpected error occurred: $($_.Exception.Message)"
-    }
-}
-
-
-function Get-MyFileContent {
-    [CmdletBinding()]
-    param(
-        [Parameter(Mandatory=$true)]
-        [string]$FilePath
-    )
-    try {
-        $content = Get-Content $FilePath -ErrorAction Stop
-        return $content
-    }
-    catch {
-        Write-Error "Failed to read file content. Error: $($_.Exception.Message)"
     }
 }
